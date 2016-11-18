7666099f52c71343e4e5172da59ea8fd4a78b462
kaznak <kaznak@gmail.com>
2016-11-18 22:56:20 +0900

    Add init/09.package.el

---
+++ init/09.package.el
@@ -0,0 +1,12 @@
+;;; 20.package.el --- Emacs Package system Configuration
+
+(require 'package)
+
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(setq load-path (cons (expand-file-name "~/.emacs.d/elpa") load-path))
+
+(package-initialize)
