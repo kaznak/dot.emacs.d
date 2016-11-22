3a15813e11969fd559e5dbb3ca2ad620a9cf68ad
kaznak <kaznak@gmail.com>
2016-11-22 22:13:53 +0900

    Update init/30.web-mode.el

--- init/30.web-mode.el
+++ init/30.web-mode.el
@@ -1,4 +1,5 @@
 ;;; 30.web-mode.el --- web-mode configuration
 
-(require 'web-mode)
+;;(require 'web-mode)
+(autoload 'web-mode "web-mode")
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
