a996e90b28e8a634ddaed8c1554f5b5eef8e7302
kaznak <kaznak@gmail.com>
2016-12-19 11:42:46 +0900

    Move and modify from init/10.display-time.el to init/10.mode-line.el

--- init/10.mode-line.el
+++ init/10.mode-line.el
@@ -1,5 +1,10 @@
-;;; 10.display-time.el --- Display Datetime on Emacs Mode-Line
+;;; 10.mode-line.el --- Configuration of Mode-Line
 
+;; show line and column number
+(line-number-mode t)
+(column-number-mode t)
+
+;; show datetime
 (setq display-time-24hr-format t)
 (setq display-time-string-forms
       '((format "%s/%s/%s(%s) %s:%s" year month day dayname 24-hours minutes)) )
