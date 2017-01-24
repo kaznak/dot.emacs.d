bb8cefa6748ae7fa29d75da2c502413dbd78073b
kaznak <kaznak@gmail.com>
2017-01-24 12:35:38 +0900

    comment out marmalade server due to SSL warning

--- setup.el
+++ setup.el
@@ -23,7 +23,7 @@
 
 (when (require 'package)
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-  (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
+  ;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
