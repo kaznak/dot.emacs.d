e9f0126ae062eeebbfe1551bbcc41f96e8ca7029
kaznak <kaznak.at.work@gmail.com>
2018-02-08 10:33:02 +0900

    feat(zeal-at-point): add configuration .el
    SSIA

---
+++ init/50.zeal-at-point.el
@@ -0,0 +1,19 @@
+;;; 50.zeal-at-point.el --- Search the word at point with Zeal
+;;; https://github.com/jinzhu/zeal-at-point
+
+(autoload 'zeal-at-point "zeal-at-point")
+
+(global-set-key "\C-cd" 'zeal-at-point)
+
+;; narrow down with a docset prefix
+(add-to-list 'zeal-at-point-mode-alist '(perl-mode . "perl"))
+;; ;; Use multiple docsets
+(add-to-list 'zeal-at-point-mode-alist '(python-mode . ("python" "django")))
+
+;; ;; set buffer local zeal-at-point-docset
+;; (add-hook 'rinari-minor-mode-hook
+;;    (lambda () (setq zeal-at-point-docset "rails")))
+;; ;; ;; Use multiple docsets
+;; (add-hook 'python-mode-hook
+;;    (lambda () (setq zeal-at-point-docset '("python" "django"))))
+
