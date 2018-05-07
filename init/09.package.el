f67d59822c818e60b0df67fc45a344481409764c
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2018-05-07 16:35:35 +0900

    feat(package): add melpa-stable to package-archives

--- init/09.package.el
+++ init/09.package.el
@@ -5,6 +5,8 @@
 (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 ;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 (setq load-path (cons (expand-file-name "~/.emacs.d/elpa") load-path))
