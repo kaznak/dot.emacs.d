f57609974e2aca418c35435d9af8e638ec92f8cb
Kazutaka Nakamura <kaznak.at.work@gmail.com>
2019-01-04 11:05:02 +0900

    feat(rjsx-mode): add setup script

---
+++ init/30.rjsx-mode.el
@@ -0,0 +1,5 @@
+;;; 30.rjsx-mode.el
+
+(add-to-list 'auto-mode-alist '(".*\\.js\\'" . rjsx-mode))
+;;(add-to-list 'auto-mode-alist '("components\\/.*\\.js\\'" . rjsx-mode))
+;;(add-to-list 'auto-mode-alist '("containers\\/.*\\.js\\'" . rjsx-mode))
